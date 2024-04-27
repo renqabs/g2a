574fbe8be9e0f9bca52f85dc15daed9a6f77c8bc
renqabs <130155002+renqabs@users.noreply.github.com>
2024-04-27 10:51:41 +0800

    Update router.go

--- initialize/router.go
+++ initialize/router.go
@@ -18,7 +18,7 @@ func InitRouter() *gin.Engine {
 	Router.GET("/ping", func(c *gin.Context) {
 		c.String(200, "(｡•ˇ‸ˇ•｡)哼！都怪你\n(`ȏ´) 也不哄哄人家\n(〃′o`)人家超想哭的，捶你胸口，大坏蛋！！！\n(｡•ˇ‸ˇ•｡)哼！都怪你\n(`ȏ´)也不哄哄人家\n(〃′o`)人家超想哭的，捶你胸口，大坏蛋！！！\n(￣^￣)ゞ咩QAQ捶你胸口你好讨厌！\n(￣^￣)ゞ咩QAQ捶你胸口你好讨厌！\n(=ﾟωﾟ)ﾉ要抱抱嘤嘤嘤哼，要抱抱嘤嘤嘤哼，人家拿小拳拳捶你胸口！！！\n(=ﾟωﾟ)ﾉ要抱抱嘤嘤嘤哼，要抱抱嘤嘤嘤哼，人家拿小拳拳捶你胸口！！！\n(｡• ︿•̀｡)大坏蛋，打死你(つд⊂)")
 	})
-	v1Group := Router.Group("/v1/")
+	v1Group := Router.Group("/yyds/v1/")
 	router.InitRouter(v1Group)
 
 	return Router
